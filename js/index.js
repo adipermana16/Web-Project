f3d24f7cb4b2afb0f0d3be2e1594b5b7c4a77a4b
adipermana16 <adiemokonkz16@gmail.com>
2025-01-20 21:44:16 -0800

    menambahkan card

---
+++ js/index.js
@@ -0,0 +1,12 @@
+var karcis = true
+var tiket = false
+
+if(karcis){
+    if(karcis){
+        console.log("masuk tampa denda")
+        }
+    }else if(tiket){
+        console.log("masuk dengan denda")
+    } else{
+        console.log("dihantam")
+    }
